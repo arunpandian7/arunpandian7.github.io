08f0778025886848009e13c5c060f4fc5ed20ecd
Arun Pandian R <arunpandian.srk@gmail.com>
2023-08-07 14:47:00 +0530

    add: preact for handling dark mode state

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -9,7 +10,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arunpandian7.github.io/portfolio',
-  integrations: [mdx(), sitemap(), tailwind(), image(
+  integrations: [preact(), mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
       cacheDir: "./.cache/image",
