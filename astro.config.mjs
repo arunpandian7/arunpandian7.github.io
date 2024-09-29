24e6fd330e6babfb0665ddd99e1c2516e3db1729
Arun Pandian R <arunpandian.srk@gmail.com>
2024-09-29 10:34:47 +0530

    update: removed @astrojs/image and other deprecated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://arunpandian7.github.io/portfolio',
-  integrations: [preact(), mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
-});
+    site: 'https://arunpandian7.github.io',
+    integrations: [
+        preact({ compat: true, debug: true }),
+        sitemap(),
+        tailwind(),
+    ],
+})
+
